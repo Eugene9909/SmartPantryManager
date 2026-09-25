10a5cd06aae89ed57cbb37d23a63fccc5d24466e
Eugene <moleaeugene@gmail.com>
2026-09-25 20:40:02 +0200

    Increment database version to trigger recipe seeding

--- app/src/main/java/com/example/smartpantrymanager/database/DatabaseHelper.java
+++ app/src/main/java/com/example/smartpantrymanager/database/DatabaseHelper.java
@@ -11,7 +11,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "smart_pantry.db";
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 2;
 
     // Table names
     public static final String TABLE_PANTRY = "pantry_items";
